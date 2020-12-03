e1b3d2998a623e29b171e36bf0310345f2868579
FairyM <1641503342@qq.com>
2020-12-03 19:26:35 +0800

    上传项目

---
+++ pages/forget/forget.vue
@@ -0,0 +1,133 @@
+<template>
+	<view class="message">
+		<view class="input-group">
+			<view class="input-row">
+				<image class="icon-phone" src="../../static/icon-phone@2x.png"></image>
+				<input class="m-input" type="text" maxlength='11' placeholder="请输入手机号"></input>
+			</view>
+		</view>
+		<view class="xian"></view>
+		<view class="input-group">
+			<view class="input-row">
+				<image class="icon-yzm" src="../../static/icon-yzm@2x.png"></image>
+				<input class="m-input" type="text" maxlength='6' placeholder=" 请输入验证码"></input>
+				<button class="bton2" plain="true" type="warn">获取验证码</button>
+			</view>
+		</view>
+		<view class="xian"></view>
+		<view class="input-group">
+			<view class="input-row">
+				<image class="icon-suo" src="../../static/icon-suo@2x.png"></image>
+				<input class="m-input" type="text" placeholder=" 新密码" password="true"></input>
+			</view>
+		</view>
+		<view class="xian"></view>
+		<view class="input-group">
+			<view class="input-row">
+				<image class="suo-2" src="../../static/icon-suo@2x.png"></image>
+				<input class="m-input" type="text" placeholder=" 确认密码" password="true"></input>
+			</view>
+		</view>
+		<view class="xian"></view>
+		<view class="btn-row">
+			<button class="bton" open-type="openSetting">
+				<image src="../../static/圆角矩形4@2x.png"></image>
+				<view class="sure" style="color: #FFFFFF;">确定</view>
+			</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+	
+		},
+		methods: {
+	
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20rpx;
+		font-size: 14rpx;
+		line-height: 24rpx;
+	}
+	.icon-phone {
+		width: 27rpx;
+		height: 41rpx;
+	}
+	.icon-yzm {
+		width: 28rpx;
+		height: 24rpx;
+	}
+	.icon-suo {
+		width: 28rpx;
+		height: 38rpx;
+	}
+	.suo-2 {
+		width: 28rpx;
+		height: 38rpx;
+	}
+	.bton{
+		width: 570rpx;
+		height: 88rpx;
+		border-radius: 44rpx;
+		background-color: #eb9229;
+		display: flex;
+		margin-top: 150rpx;
+		border: 0;
+	}
+	.bton2 {
+		width: 200rpx;
+		height: 70rpx;
+		border: solid 1rpx #eb9229;
+		border-radius:25rpx;
+		font-family: PingFang-SC-Regular;
+		font-size: 28rpx;
+		font-weight: normal;
+		font-stretch: normal;
+		letter-spacing: 0rpx;
+	}
+	.input-row{
+		display: flex;
+		flex-direction: row;
+		margin-left: 60rpx;
+		margin-top: 98rpx;
+	}
+	.m-input {
+		width: 165rpx;
+		height: 26rpx;
+		font-family: PingFangSC-Regular;
+		font-size: 28rpx;
+		font-weight: normal;
+		font-stretch: normal;
+		letter-spacing: 0rpx;
+		color: #cccccc;
+	}
+	.xian {
+		width: 630rpx;
+		height: 1rpx;
+		background-color: #eeeeee;
+		margin-left: 60rpx;
+		margin-right: 60rpx;
+	}
+	.sure {
+		position: absolute;
+		width: 300rpx;
+		height: 34rpx;
+		font-family: PingFangSC-Regular;
+		font-size: 45rpx;
+		font-weight: normal;
+		font-stretch: normal;
+		letter-spacing: 0rpx;
+		color: #ffffff;
+		margin-left: 100rpx;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,183 @@
+<template>
+	<view class="month">
+		<view class="tx-list">
+			<view class="thismonth">本月</view>
+			<view class="tx-item">
+				<view class="qian">
+					<view class="theday">昨天</view>
+					<view class="thetime">09:00</view>
+				</view>
+				<view class="zfb">转至支付宝账户</view>
+				<view class="hou">
+					<view class="money">230.00元</view>
+					<view class="state" style="color: #ff8e16;">提现中</view>
+				</view>
+			</view>
+			<view class="xian"></view>
+			<view class="tx-item">
+				<view class="qian">
+					<view class="theday">昨天</view>
+					<view class="thetime">09:00</view>
+				</view>
+				<view class="wx">转至微信账户</view>
+				<view class="hou">
+					<view class="money">230.00元</view>
+					<view class="state">已到账</view>
+				</view>
+			</view>
+			<view class="xian"></view>
+			<view class="tx-item">
+				<view class="qian">
+					<view class="theday">昨天</view>
+					<view class="thetime">09:00</view>
+				</view>
+				<view class="wx">转至微信账户</view>
+				<view class="hou">
+					<view class="money">230.00元</view>
+					<view class="state">已到账</view>
+				</view>
+			</view>
+			<view class="xian"></view>
+			<view class="tx-item">
+				<view class="qian">
+					<view class="theday">昨天</view>
+					<view class="thetime">09:00</view>
+				</view>
+				<view class="zfb">转至支付宝账户</view>
+				<view class="hou">
+					<view class="money">230.00元</view>
+					<view class="state">已到账</view>
+				</view>
+			</view>
+			<view class="xian"></view>
+		</view>
+		<view class="thismonth">六月</view>
+		<view class="tx-list">
+			<view class="tx-item">
+				<view class="qian">
+					<view class="theday">昨天</view>
+					<view class="thetime">09:00</view>
+				</view>
+				<view class="zfb">转至支付宝账户</view>
+				<view class="hou">
+					<view class="money">230.00元</view>
+					<view class="state">已到账</view>
+				</view>
+			</view>
+			<view class="xian"></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.thismonth {
+		width: 400px;
+		height: 28px;
+		color: #666666;
+		background-color: #d6d6e2;
+		padding-left: 25rpx;
+		opacity: 0.2;
+	}
+	.theday {
+		width: 100px;
+		height: 32px;
+		font-family: PingFang-SC-Medium;
+		font-size: 17px;
+		font-weight: normal;
+		font-stretch: normal;
+		line-height: 36px;
+		letter-spacing: 0px;
+		color: #666666;
+		margin-left: 38rpx;
+	}
+	.thetime {
+		width: 68px;
+		height: 21px;
+		font-family: PingFang-SC-Medium;
+		font-size: 13px;
+		font-weight: normal;
+		font-stretch: normal;
+		line-height: 36px;
+		letter-spacing: 0px;
+		color: #999999;
+		margin-left: 36rpx;
+	}
+	.zfb {
+		width: 350rpx;
+		height: 32rpx;
+		display: flex;
+		align-content: center;
+		font-family: PingFang-SC-Medium;
+		font-size: 34rpx;
+		font-weight: normal;
+		font-stretch: normal;
+		line-height: 36px;
+		letter-spacing: 0px;
+		color: #666666;
+	}
+	.wx {
+		width: 350rpx;
+		height: 32rpx;
+		font-family: PingFang-SC-Medium;
+		font-size: 34rpx;
+		font-weight: normal;
+		font-stretch: normal;
+		line-height: 36px;
+		letter-spacing: 0px;
+		color: #666666;
+	}
+	.money {
+		width: 100px;
+		height: 30px;
+		text-align: right;
+		padding-right: 2px;
+		font-family: PingFang-SC-Medium;
+		font-size: 17px;
+		font-weight: normal;
+		font-stretch: normal;
+		line-height: 36px;
+		letter-spacing: 0px;
+		color: #666666;
+	}
+	.state {
+		text-align: right;
+		padding-right: 2px;
+		font-family: PingFang-SC-Medium;
+		font-size: 15px;
+		font-weight: normal;
+		font-stretch: normal;
+		line-height: 36px;
+		color: #999999;
+	}
+	.qian{
+		display: flex;
+		flex-direction: column;
+	}
+	.hou{
+		margin-right: 10rpx;
+		display: flex;
+		flex-direction: column;
+	}
+	.tx-item{
+		display: flex;
+		flex-direction: row;
+	}
+	.xian {
+		width: 630px;
+		height: 1px;
+		background-color: #eeeeee;
+	}
+</style>
